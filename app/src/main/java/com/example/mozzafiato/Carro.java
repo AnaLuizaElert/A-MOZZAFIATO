c1d8aa5ed2df408b0c262234e6ee76e8e80d462b
annabel30 <annabel_wulf@estudante.sesisenai.org.br>
2023-07-12 21:50:32 -0300

    First commit

---
+++ app/src/main/java/com/example/mozzafiato/Carro.java
@@ -0,0 +1,14 @@
+package com.example.mozzafiato;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Carro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carro);
+    }
+}
---
+++ app/src/main/java/com/example/mozzafiato/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.mozzafiato;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(MainActivity.this, Carro.class);
+        startActivity(intent);
+        finish();
+    }
+}
